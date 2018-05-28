74b4fc86c2719e64f9811c0009dda1f41b10aa71
jernejcernelc <jernej.cernelc@gmail.com>
2018-05-28 19:10:08 +0200

    fixed sidebar

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-    
+
     /* set new icons for languages */
     var eng = "https://image.flaticon.com/icons/svg/197/197484.svg";
     var slo = "https://image.flaticon.com/icons/svg/197/197633.svg";
@@ -42,7 +42,14 @@ window.onload = function(){
     /* change order of sidebar and body */
     var body = document.getElementById("body")
     body.insertBefore(body.childNodes[3], body.childNodes[1]);
-    
+
+    /* change order of sidebar elements */
+    var rightSidebar = document.getElementById("rightSidebar");
+    console.log(rightSidebar.childNodes);
+	swapElements(rightSidebar.childNodes[10], rightSidebar.childNodes[8]);
+	swapElements(rightSidebar.childNodes[19], rightSidebar.childNodes[16]);
+	
+	wrap([rightSidebar.childNodes[10], rightSidebar.childNodes[16], rightSidebar.childNodes[19]]);
     
     /* Scroll position for navbar */
     var height = document.documentElement.scrollHeight
@@ -103,4 +110,36 @@ window.onload = function(){
         	document.getElementById("sidebarHelp").getElementsByTagName("a")[0].style.top = "22%";
         }
     });
+}
+
+function swapElements(obj1, obj2) {
+    // create marker element and insert it where obj1 is
+    var temp = document.createElement("div");
+    obj1.parentNode.insertBefore(temp, obj1);
+
+    // move obj1 to right before obj2
+    obj2.parentNode.insertBefore(obj1, obj2);
+
+    // move obj2 to right before where obj1 used to be
+    temp.parentNode.insertBefore(obj2, temp);
+
+    // remove temporary marker node
+    temp.parentNode.removeChild(temp);
+}
+
+function wrap(nodesToWrap) {
+	var addedToDocument = false;
+	var wrapper = document.createElement("div");
+	wrapper.className = "block";
+	for (var index = 0; index < nodesToWrap.length; index++) {
+	    var node = nodesToWrap[index];
+	    node.className = "wrappedBlock";
+	    if (! addedToDocument) {
+	        node.parentNode.insertBefore(wrapper, node);
+	        addedToDocument = true;
+	    }
+	    node.parentNode.removeChild(node);
+	    wrapper.appendChild(node);
+	}
+	return wrapper;
 }
